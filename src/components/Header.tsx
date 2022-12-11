e8b0b1a73ad24c297514ab657c6b436621ab2a10
Faukkirik <kote_33419@mai.ru>
2022-12-11 21:41:09 +0300

    3 commit - css-modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from './Header.module.css'
 
 export const Header = () => {
-    return <header className={"header"}>
+    return <header className={s.header}>
         <img
             src={"https://avatars.mds.yandex.net/i?id=5e0babbe5aeabe4e31016651e20a4e36240027da-7662303-images-thumbs&n=13"}/>
     </header>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 export const Profile = () => {
     return (
-        <div className={"content"}>
+        <div className={s.content}>
         <div>
             <img
                 src={"https://avatars.mds.yandex.net/i?id=8e898d061411b4c4846cee67b1d4e064e95786c6-4484124-images-thumbs&n=13"}/>
@@ -15,11 +16,11 @@ export const Profile = () => {
             <div>
                 New post
             </div>
-            <div>
-                <div>
+            <div className={s.posts}>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
