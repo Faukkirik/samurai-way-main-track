07abfccdd51f476d59dd5ddafc38109f09734697
Faukkirik <kote_33419@mai.ru>
2022-12-17 12:48:53 +0300

    6 commit - str Dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img
                     src={"https://avatars.mds.yandex.net/i?id=8e898d061411b4c4846cee67b1d4e064e95786c6-4484124-images-thumbs&n=13"}/>
