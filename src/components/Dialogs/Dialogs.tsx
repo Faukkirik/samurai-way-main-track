5861055755ecec74440b96e144dc11745f32d5cb
Faukkirik <kote_33419@mai.ru>
2022-12-17 18:09:53 +0300

    9 commit - werstka dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-     Dialogs
-    </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Жека
+                </div>
+                <div className={s.dialog}>
+                    Даник
+                </div>
+                <div className={s.dialog}>
+                    Борис
+                </div>
+                <div className={s.dialog}>
+                    Никита
+                </div>
+                <div className={s.dialog}>
+                    Каменщик 4 разряда
+                </div>
+                <div className={s.dialog}>
+                    Каменщик 5 разряда
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>HI</div>
+                <div className={s.message}>Bue</div>
+                <div className={s.message}>Yo</div>
+            </div>
+        </div>
     )
 }
