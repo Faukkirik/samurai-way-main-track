cc0fedccbbca3bcc7d2763fb04bdabd4bd2345f8
Faukkirik <kote_33419@mai.ru>
2022-12-19 13:04:08 +0300

    12 commit - map / massive

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -24,7 +24,7 @@ const Message = (props: PropsTypeMessage) => {
         <div className={s.dialog}>{props.message}</div>
     )
 }
-export const Dialogs = (props: PropsTypeUsers) => {
+export const Dialogs = (props: PropsTypeUsers ) => {
     let dialogsData = [
         {id: 1, name: "Женя"},
         {id: 2, name: "Даник"},
@@ -33,6 +33,9 @@ export const Dialogs = (props: PropsTypeUsers) => {
         {id: 5, name: "Каменщик"},
         {id: 6, name: "Бетонщик"},
     ]
+    let dialogsElements = dialogsData.map( (dialog) => {
+        return (<DialogItem name={dialog.name} id={dialog.id}/>)
+    })
     let messagesData = [
         {id: 1, message: "Hi"},
         {id: 2, message: "Yo"},
@@ -40,20 +43,17 @@ export const Dialogs = (props: PropsTypeUsers) => {
         {id: 4, message: "Hello"},
         {id: 5, message: "<>!&7r>"},
     ]
+    let messagesElements = messagesData.map( (message) => {
+        return (<Message message={message.message}/>)
+    })
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
+                {messagesElements}
+                {/*<Message message={messagesData[0].message}/>*/}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,7 +3,6 @@ import s from './MyPosts.module.css';
 import {Post} from "./Post/Post";
 
 
-
 /*type PropsTypePostData = {
     id?: number
     message: string
@@ -12,11 +11,12 @@ import {Post} from "./Post/Post";
 export const MyPosts = (/*props: PropsTypePostData*/) => {
     let postData = [
         {id: 1, message: "Hi, how are you?", like: 11},
-        {id: 2, message: "It's my first post." , like: 27},
-        {id: 3, message: "It's my first post." , like: 21},
-        {id: 4, message: "It's my first post." , like: 21},
-        {id: 5, message: "It's my first post." , like: 21},
+        {id: 2, message: "It's my first post.", like: 27},
+        {id: 3, message: "Oh, nooo.", like: 21},
     ]
+    let postsElements = postData.map((post) => {
+        return (<Post message={post.message} like={post.like} id={post.id}/>)
+    })
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -31,8 +31,8 @@ export const MyPosts = (/*props: PropsTypePostData*/) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} like={postData[0].like} id={postData[0].id}/>
-                <Post message={postData[1].message} like={postData[1].like} id={postData[1].id}/>
+                {postsElements}
+                {/*<Post message={postData[0].message} like={postData[0].like} id={postData[0].id}/>*/}
             </div>
         </div>)
 }
